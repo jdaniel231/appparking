00607bd3423d33463cc17ef84c7397ceb8f0fffc
hammer200911 <jdaniel231@gmail.com>
2023-01-19 11:00:34 -0300

    navbar, footer

---
+++ src/components/Footer.vue
@@ -0,0 +1,14 @@
+<template>
+
+  <p>Sistema de Estacionamento 2023 - Desenvolvimento por Joao Daniel</p>
+</template>
+
+<script>
+export default {
+  name: "Footer"
+}
+</script>
+
+<style scoped>
+
+</style>
